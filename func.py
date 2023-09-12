50ef531a73315b348d5026f32d67fce6d70e5c02
Это_gambit <ars071026@gmail.com>
2023-09-12 20:54:02 +0500

    первая часть проекта

--- func.py
+++ func.py
@@ -5,12 +5,19 @@ def open_file() -> list[str]:
     with open(FILE_NAME, "r", encoding="utf-8") as f:
         my_str = f.read()
         notion_list = my_str.split(";")
-        for i in range(len(notion_list)):
+        for i in range(len(notion_list) - 1):
             notion_list[i] = notion_list[i].strip()
+            if notion_list[i] == "":
+                notion_list.pop(i)
         return notion_list
 
 
 def save_file(user_list: list[str]):
-    with open(FILE_NAME,"w", encoding="utf-8") as f:
+    with open(FILE_NAME, "w", encoding="utf-8") as f:
         for i in range(len(user_list)):
-            f.write(user_list[i] + "\n")
+            f.write(user_list[i] + ";" + "\n")
+
+
+def print_notion(user_list: list[str]):
+    for i in range(len(user_list)):
+        print(f"{i + 1}." + user_list[i])
--- model.py
+++ model.py
@@ -6,8 +6,8 @@ def start_notion():
     flag = True
     notion_list = 0
     while flag:
-        event = input(event_option)
-        if event.isdigit() or 0 < int(event) < 6:
+        event = input(event_option).strip()
+        if event.isdigit() and 0 < int(event) < 7:
             event = int(event)
         else:
             print(input_error)
@@ -15,22 +15,61 @@ def start_notion():
         match event:
             case 1:
                 notion_list = open_file()
+                print(len(notion_list[2]))
                 print(mission_completed)
             case 2:
                 if notion_list == 0:
                     print(open_error)
                     continue
-                notion = input(new_notion)
+                notion = input(new_notion).strip()
                 if notion == "":
                     print(input_error)
                     continue
-                notion_list.append(notion + ";")
+                notion_list.append(notion)
                 print(mission_completed)
             case 3:
-                pass
+                if notion_list == 0:
+                    print(open_error)
+                    continue
+                elif len(notion_list) == 0:
+                    print(empty_error)
+                else:
+                    print_notion(notion_list)
+                    deli = input(del_notion).strip()
+                    if deli.isdigit() and 0 < int(deli) < len(notion_list) + 1:
+                        notion_list.pop(int(deli) - 1)
+                        print(mission_completed)
+                    else:
+                        print(input_error)
             case 4:
-                pass
+                if notion_list == 0:
+                    print(open_error)
+                    continue
+                elif len(notion_list) == 0:
+                    print(empty_error)
+                else:
+                    print_notion(notion_list)
+                    edit = input(edit_notion).strip()
+                    if edit.isdigit() and 0 < int(edit) < len(notion_list) + 1:
+                        notion = input(new_notion).strip()
+                        if notion == "":
+                            print(input_error)
+                            continue
+                        else:
+                            notion_list[int(edit) - 1] = notion
+                            print(mission_completed)
+                    else:
+                        print(input_error)
             case 5:
+                if notion_list == 0:
+                    print(open_error)
+                    continue
+                elif len(notion_list) == 0:
+                    print(empty_error)
+                    continue
+                else:
+                    print_notion(notion_list)
+            case 6:
                 flag = False
                 if notion_list == 0:
                     print(good_bye)
